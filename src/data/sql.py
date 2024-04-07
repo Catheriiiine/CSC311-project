e2cb5044ca04832fd43c82792e90f41b34e9b728
youyang <youyang.guo@mail.utoronto.ca>
2024-04-07 00:00:10 -0400

    sql

--- src/data/sql.py
+++ src/data/sql.py
@@ -5,59 +5,17 @@ import ast
 train_data = pd.read_csv('train_data.csv')
 question_meta = pd.read_csv('question_meta.csv')
 subject_meta = pd.read_csv('subject_meta.csv')
-student_meta = pd.read_csv('student_meta.csv')
 valid_data = pd.read_csv('valid_data.csv')
 test_data = pd.read_csv('test_data.csv')
 
 conn = sqlite3.connect(':memory:')
+
 train_data.to_sql('train_data', conn, index=False, if_exists='replace')
 question_meta.to_sql('question_meta', conn, index=False, if_exists='replace')
 subject_meta.to_sql('subject_meta', conn, index=False, if_exists='replace')
-student_meta.to_sql('student_meta', conn, index=False, if_exists='replace')
 valid_data.to_sql('valid_data', conn, index=False, if_exists='replace')
 test_data.to_sql('test_data', conn, index=False, if_exists='replace')
 
-# # Convert the string representation of lists in 'subject_id' to actual lists
-# question_meta['subject_id'] = question_meta['subject_id'].apply(ast.literal_eval)
-#
-# # Explode the 'subject_id' list into separate rows
-# exploded_question_meta = question_meta.explode('subject_id')
-#
-# # Convert 'subject_id' in exploded_question_meta to int for proper comparison
-# exploded_question_meta['subject_id'] = exploded_question_meta['subject_id'].astype(int)
-#
-# subject_meta['subject_id'] = subject_meta['subject_id'].astype(int)
-#
-# # Create a flag in exploded_question_meta to indicate presence
-# exploded_question_meta['present'] = 1
-#
-# all_combinations = (
-#     question_meta[['question_id']]
-#     .assign(key=1)
-#     .merge(subject_meta.assign(key=1), how='outer', on='key')
-#     .drop('key', axis=1)
-#     .rename(columns={'subject_id': 'sid'})
-# )
-#
-# # Mark with 1 where a subject_id is associated with a question_id, else 0
-# all_combinations['value'] = all_combinations.apply(lambda x: 1 if x['sid'] in question_meta.loc[question_meta['question_id'] == x['question_id'], 'subject_id'].values[0] else 0, axis=1)
-#
-#
-# # Merge all_combinations with exploded_question_meta to mark presence
-# optimized_combinations = all_combinations.merge(
-#     exploded_question_meta[['question_id', 'subject_id', 'present']],
-#     left_on=['question_id', 'sid'],
-#     right_on=['question_id', 'subject_id'],
-#     how='left'
-# ).drop(['subject_id'], axis=1).fillna(0)
-#
-# # Pivot this optimized combination DataFrame
-# optimized_pivoted_table = optimized_combinations.pivot_table(index='question_id', columns='sid', values='present', fill_value=0).reset_index()
-# if 'sid' in optimized_pivoted_table.columns:
-#     optimized_pivoted_table = optimized_pivoted_table.drop(columns=['sid'])
-# # Show the first few rows of the optimized pivoted table
-# print(optimized_pivoted_table.head())
-# optimized_pivoted_table.to_csv('question_subject.csv', index=False)
 question_subject = pd.read_csv('question_subject.csv')
 question_subject.to_sql('question_subject', conn, index=False, if_exists='replace')
 
--- src/data/sql3.py
+++ src/data/sql3.py
@@ -11,12 +11,12 @@ pivoted_data.reset_index(inplace=True)
 
 pivoted_data.to_csv('question_correctness.csv', index=False)
 
-pivoted_data1 = (train_data.pivot(index='user_id', columns='question_id', values='is_correct'))
+pivoted_data1 = (valid_data.pivot(index='user_id', columns='question_id', values='is_correct'))
 pivoted_data1.reset_index(inplace=True)
 
 pivoted_data1.to_csv('question_correctness1.csv', index=False)
 
-pivoted_data2 = (train_data.pivot(index='user_id', columns='question_id', values='is_correct'))
+pivoted_data2 = (test_data.pivot(index='user_id', columns='question_id', values='is_correct'))
 pivoted_data2.reset_index(inplace=True)
 
 pivoted_data2.to_csv('question_correctness2.csv', index=False)
